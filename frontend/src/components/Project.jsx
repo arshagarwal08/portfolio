2037e27ba996405d66e4bf0dcd5351d15771d1fa
Arsh Agarwal <arshagarwal08@gmail.com>
2025-04-30 12:16:02 +0530

    Final commit

--- frontend/src/components/Project.jsx
+++ frontend/src/components/Project.jsx
@@ -6,26 +6,32 @@ import axios from 'axios';
 import { SiJavascript, SiReact, SiNodedotjs, SiMongodb, SiPython } from 'react-icons/si';
 
 const Projects = () => {
-  const { baseUrl } = useApi();
-  const [projects, setProjects] = useState([]);
+  
+  const projects = [
+    {"_id":{"$oid":"6807c55bc0caccfde1bbdd84"},"title":"Foodie (Zomato/Swiggy clone)","description":"A food delivery app that allows users to browse local restaurants, explore dynamic menus, place orders, and track order status. Foodie offers a seamless and convenient dining experience.","technologies":["React","Node.js","Express.js","Tailwind CSS","MongoDB"],"repoUrl":"https://github.com/arshagarwal08/foodStore.git","imageUrl":"https://i.postimg.cc/7PVZ5QZt/Screenshot-2025-04-22-001335.png"},
+    {"_id":{"$oid":"6807c80bc0caccfde1bbdd88"},"title":"Portfolio Website","description":"A personal portfolio website to showcase my skills and projects. Designed using React and Tailwind CSS, this website presents my work and provides a way for others to connect with me.","technologies":["React","Tailwind CSS","Node.js","Express.js","MongoDB"],"imageUrl":"https://i.postimg.cc/sDBdYj2T/Screenshot-2025-04-30-110039.png","repoUrl":"https://github.com/arshagarwal08/portfolio.git"},
+    {"_id":{"$oid":"6807c949c0caccfde1bbdd8b"},"title":"BookVerse","description":"An online bookstore where users can browse books. Features include user authentication, browsing by categories, and light/dark mode. Built with the MERN stack.","technologies":["React","Node.js","MongoDB","Express.js","Tailwind CSS"],"repoUrl":"https://github.com/arshagarwal08/bookStore-project.git","imageUrl":"https://i.postimg.cc/Bvz4TdK4/Screenshot-2025-04-22-222017.png"}
+  ]  
+  
+  // const { baseUrl } = useApi();
+  // const [projects, setProjects] = useState([]);
+  // useEffect(() => {
+  //   const fetchProjects = async () => {
+  //     try {
+  //       const response = await axios.get(baseUrl + "/project");
+  //       if (response.data.success) {
+  //         setProjects(response.data.data);
+  //       } else {
+  //         alert(response.data.message);
+  //       }
+  //     } catch (error) {
+  //       console.log(error);
+  //       alert("Some error occurred");
+  //     }
+  //   };
 
-  useEffect(() => {
-    const fetchProjects = async () => {
-      try {
-        const response = await axios.get(baseUrl + "/project");
-        if (response.data.success) {
-          setProjects(response.data.data);
-        } else {
-          alert(response.data.message);
-        }
-      } catch (error) {
-        console.log(error);
-        alert("Some error occurred");
-      }
-    };
-
-    fetchProjects();
-  }, [baseUrl]);
+  //   fetchProjects();
+  // }, [baseUrl]);
 
   const techIcons = {
     'React': <SiReact className="text-blue-400" />,
